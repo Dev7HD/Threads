cd452f49e87f3ab1d63bb48ceb622c353530f7b3
Hamza Damiri <122821566+Dev7HD@users.noreply.github.com>
2024-03-02 16:55:46 +0100

    Update Talkative.java

--- src/main/java/Exercice1/Talkative.java
+++ src/main/java/Exercice1/Talkative.java
@@ -9,7 +9,7 @@ public class Talkative implements Runnable {
 
     @Override
     public void run() {
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 100; i++) {
             System.out.println(nb);
         }
     }
